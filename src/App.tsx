f6f3f7d336dbf608de124ee9cbd0762b86c21041
carlosberti <carlosberti182@gmail.com>
2023-08-24 12:43:12 -0300

    Fix App test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Home } from './pages/Home';
-import { Header } from './components/Header';
+import { Home } from '@/pages';
+import { Header } from '@/components';
 
 const queryClient = new QueryClient();
 
 function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
+			learn react
 			<Header />
 			<Home />
 		</QueryClientProvider>
