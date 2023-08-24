5a4a4dfbc1f34260cdc616b03298ea23846f7ed7
carlosberti <carlosberti182@gmail.com>
2023-08-24 09:17:45 -0300

    Add prettier packages and initial config with eslint and editorconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <div className="bg-red-50">
-      <h1 className="text-lg">test</h1>
-    </div>
-  );
+	return (
+		<div className="bg-red-50">
+			<h1 className="text-lg">test</h1>
+		</div>
+	);
 }
 
 export default App;
