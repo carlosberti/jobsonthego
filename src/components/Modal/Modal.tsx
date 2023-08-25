6edda80d96f1bc1545992a0b3e3169e5ec246a40
carlosberti <carlosberti182@gmail.com>
2023-08-25 17:06:12 -0300

    Add some page interactions and toast message

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,12 +1,23 @@
 import { getJobsList } from '@/actions';
 import { useQuery } from '@tanstack/react-query';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import * as Dialog from '@radix-ui/react-dialog';
+import * as VisuallyHidden from '@radix-ui/react-visually-hidden';
+import * as Toast from '@radix-ui/react-toast';
+import { LinkedInLogoIcon, Share1Icon } from '@radix-ui/react-icons';
 import DOMPurify from 'dompurify';
+import { useEffect, useRef, useState } from 'react';
 
 export function Modal() {
+	const navigate = useNavigate();
 	const { id } = useParams<{ id?: string }>();
 	const { isLoading, error, data } = useQuery(['jobsList'], getJobsList);
+	const [open, setOpen] = useState(false);
+	const timerRef = useRef(0);
+
+	useEffect(() => {
+		return () => clearTimeout(timerRef.current);
+	}, []);
 
 	if (isLoading) return 'Loading...';
 
@@ -27,61 +38,102 @@ export function Modal() {
 	const sanitizedSkills = DOMPurify.sanitize(job.skills);
 
 	return (
-		<Dialog.Root open>
-			<Dialog.Portal>
-				<Dialog.Overlay className="absolute inset-0 bg-gray-200/5 backdrop-blur-sm" asChild>
-					<Dialog.Close asChild>
-						<Link to="/" />
-					</Dialog.Close>
-				</Dialog.Overlay>
-				<Dialog.Content className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-auto border-gray-200 bg-white focus:outline-none sm:bottom-16 sm:left-[50%] sm:top-16 sm:h-[initial] sm:w-[80%] sm:max-w-4xl sm:translate-x-[-50%] sm:rounded-md sm:border sm:drop-shadow-md">
-					{job.company.cover && (
-						<img
-							src={job.company.cover}
-							alt="Company cover"
-							height={256}
-							className="relative h-full max-h-64 w-full rounded-t-md object-cover"
-						/>
-					)}
-					<div className="p-6">
-						<div className="mb-4 flex items-center gap-3">
-							{job.company.logo && (
-								<img src={job.company.logo} alt="Company logo" width={32} height={32} />
-							)}
-							<Dialog.Title className="text-2xl font-semibold text-zinc-900">
-								{job.title}
-							</Dialog.Title>
-						</div>
-						<div className="flex flex-col gap-5 [&_ul]:list-disc [&_ul_li]:ml-4">
-							<div
-								className="flex flex-col gap-2 font-medium text-zinc-900"
-								dangerouslySetInnerHTML={{ __html: sanitizedCompanyDescription }}
-							/>
-							<div
-								className="flex flex-col gap-2 font-medium text-zinc-900"
-								dangerouslySetInnerHTML={{ __html: sanitizedJobDescription }}
-							/>
-							<div
-								className="flex flex-col gap-2 font-medium text-zinc-900"
-								dangerouslySetInnerHTML={{ __html: sanitizedSkills }}
-							/>
-						</div>
-						<Link
-							to={job.urls.ad}
-							target="_blank"
-							rel="noopener"
-							className="mt-4 inline-block rounded-md border border-zinc-900 p-2 font-medium text-zinc-900 transition-colors hover:bg-zinc-900 hover:text-white"
-						>
-							Apply now
-						</Link>
+		<>
+			<Dialog.Root open>
+				<Dialog.Portal>
+					<Dialog.Overlay className="absolute inset-0 bg-gray-200/5 backdrop-blur-sm" asChild>
 						<Dialog.Close asChild>
-							<Link to="/" className="absolute right-4 top-4">
-								X
-							</Link>
+							<Link to="/" />
 						</Dialog.Close>
-					</div>
-				</Dialog.Content>
-			</Dialog.Portal>
-		</Dialog.Root>
+					</Dialog.Overlay>
+					<Dialog.Content
+						onEscapeKeyDown={() => navigate('/')}
+						className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-auto border-gray-200 bg-white focus:outline-none sm:bottom-16 sm:left-[50%] sm:top-16 sm:h-[initial] sm:w-[80%] sm:max-w-4xl sm:translate-x-[-50%] sm:rounded-md sm:border sm:drop-shadow-md"
+					>
+						{job.company.cover && (
+							<img
+								src={job.company.cover}
+								alt="Company cover"
+								height={256}
+								className="relative h-full max-h-64 w-full rounded-t-md object-cover"
+							/>
+						)}
+						<div className="p-6">
+							<div className="mb-4 flex items-start gap-3">
+								{job.company.logo && (
+									<img src={job.company.logo} alt="Company logo" width={32} height={32} />
+								)}
+								<Dialog.Title className="text-2xl font-semibold text-zinc-900">
+									{job.title}
+									<div className="ml-3 inline-flex gap-2 leading-8">
+										{job.linkedInCompanyId && (
+											<Link
+												to={`https://linkedin.com/company/${job.linkedInCompanyId}`}
+												target="_blank"
+												rel="noopener"
+												className="text-sky-600 transition-colors hover:text-sky-800"
+											>
+												<LinkedInLogoIcon className="h-4 w-4" />
+											</Link>
+										)}
+										<button
+											onClick={() => {
+												navigator.clipboard.writeText(window.location.href);
+												setOpen(false);
+												window.clearTimeout(timerRef.current);
+												timerRef.current = window.setTimeout(() => {
+													setOpen(true);
+												}, 100);
+											}}
+										>
+											<Share1Icon className="h-4 w-4" />
+											<VisuallyHidden.Root>Copy this job link to clipboard</VisuallyHidden.Root>
+										</button>
+									</div>
+								</Dialog.Title>
+							</div>
+							<div className="flex flex-col gap-4 [&_ul]:list-disc [&_ul_li]:ml-4">
+								<div
+									className="flex flex-col gap-3 font-medium text-zinc-900"
+									dangerouslySetInnerHTML={{ __html: sanitizedCompanyDescription }}
+								/>
+								<div
+									className="flex flex-col gap-3 font-medium text-zinc-900"
+									dangerouslySetInnerHTML={{ __html: sanitizedJobDescription }}
+								/>
+								<div
+									className="flex flex-col gap-3 font-medium text-zinc-900"
+									dangerouslySetInnerHTML={{ __html: sanitizedSkills }}
+								/>
+							</div>
+							<Link
+								to={job.urls.ad}
+								target="_blank"
+								rel="noopener"
+								className="ml-auto mt-4 block w-fit rounded-md border border-zinc-900 p-2 font-medium text-zinc-900 transition-colors hover:bg-zinc-900 hover:text-white"
+							>
+								Apply now
+							</Link>
+							<Dialog.Close asChild>
+								<Link to="/" className="absolute right-4 top-4">
+									X
+								</Link>
+							</Dialog.Close>
+						</div>
+					</Dialog.Content>
+				</Dialog.Portal>
+			</Dialog.Root>
+			<Toast.Provider duration={2000}>
+				<Toast.Root
+					className="rounded-md bg-green-300 px-6 py-4 shadow-md"
+					open={open}
+					onOpenChange={setOpen}
+					onClick={(e) => e.stopPropagation()}
+				>
+					<Toast.Description className="font-semibold text-zinc-600">Copied!</Toast.Description>
+				</Toast.Root>
+				<Toast.Viewport className="fixed bottom-0 right-0 z-50 p-3 sm:p-6" />
+			</Toast.Provider>
+		</>
 	);
 }
