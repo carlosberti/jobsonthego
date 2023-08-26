da118ccc9417a1f2f46fbc451178b82f4b382420
carlosberti <carlosberti182@gmail.com>
2023-08-26 17:46:27 -0300

    Create placeholder logo and add it to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,10 @@
-import { SearchInput } from '@/components';
+import { Logo, SearchInput } from '@/components';
 
 export function Header() {
 	return (
 		<header className="sticky top-2 m-2 flex max-w-7xl items-center gap-6 rounded-md border border-slate-100 bg-white px-4 py-2 shadow-sm sm:top-3 sm:m-3 sm:px-6">
 			<div className="sm:flex-1">
-				<h1>JobsOnTheGo</h1>
+				<Logo />
 			</div>
 			<SearchInput />
 			<div className="hidden sm:block sm:flex-1" />
