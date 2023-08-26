1f82d44e7eacb27f99eb1e992facf67006be0911
carlosberti <carlosberti182@gmail.com>
2023-08-25 21:28:55 -0300

    Change route system to support the new Form to make client side changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { BrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import { PublicRoutes } from '@/routes';
+import { publicRoutes } from '@/routes';
 
 const queryClient = new QueryClient();
 
 function App() {
 	return (
-		<BrowserRouter>
-			<QueryClientProvider client={queryClient}>
-				<PublicRoutes />
-			</QueryClientProvider>
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={publicRoutes} />
+		</QueryClientProvider>
 	);
 }
 
--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,30 +1,35 @@
-import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
+import { Navigate, Outlet, createBrowserRouter } from 'react-router-dom';
 
 import { Home } from '@/pages';
 import { Header, Modal } from '@/components';
 
-export function PublicRoutes() {
-	return (
-		<>
-			<Routes>
-				<Route
-					path="/"
-					element={
-						<>
-							<div className="mx-auto max-w-7xl sm:flex sm:min-h-[100dvh] sm:flex-col">
-								<Header />
-								<Home />
-							</div>
-							<Outlet />
-						</>
-					}
-				>
-					<Route path="/job">
-						<Route index element={<Navigate to="/" />} />
-						<Route path=":id" element={<Modal />} />
-					</Route>
-				</Route>
-			</Routes>
-		</>
-	);
-}
+export const publicRoutes = createBrowserRouter([
+	{
+		path: '/',
+		element: (
+			<>
+				<div className="mx-auto max-w-7xl sm:flex sm:min-h-[100dvh] sm:flex-col">
+					<Header />
+					<Home />
+				</div>
+				<Outlet />
+			</>
+		),
+		children: [
+			{
+				path: 'job',
+				element: <Outlet />,
+				children: [
+					{
+						index: true,
+						element: <Navigate to="/" />,
+					},
+					{
+						path: ':id',
+						element: <Modal />,
+					},
+				],
+			},
+		],
+	},
+]);
