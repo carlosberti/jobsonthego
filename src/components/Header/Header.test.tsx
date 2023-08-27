0a3dbd7e50f17c763159b882146e7ef946009454
carlosberti <carlosberti182@gmail.com>
2023-08-27 20:48:16 -0300

    Test Header

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,10 +1,20 @@
-import { renderWithRouter, screen } from '@/test-utils';
+import userEvent from '@testing-library/user-event';
+
+import { renderWithRouter, screen, act } from '@/test-utils';
 
 import { Header } from '.';
 
 describe('Header', () => {
-	test('renders Header with company name', () => {
+	test('renders Header with company name and <SearchInput />', async () => {
 		renderWithRouter(<Header />);
 		expect(screen.getAllByLabelText(/jobs on the go/i)).toHaveLength(2);
+		expect(screen.getByRole('searchbox')).toBeInTheDocument();
+	});
+	test('type at the SearchInput and submit', () => {
+		renderWithRouter(<Header />);
+
+		// eslint-disable-next-line testing-library/no-unnecessary-act
+		act(() => userEvent.type(screen.getByRole('searchbox'), 'Frontend Engineer'));
+		expect(screen.getByRole('searchbox')).toHaveValue('Frontend Engineer');
 	});
 });
